c866293c1ee5a2392d08303158d008b72aba9121
liviaolivf <livof18@gmail.com>
2024-11-22 21:35:51 -0300

    :tada: aula04-imersao-alura

--- src/controllers/posts.controller.js
+++ src/controllers/posts.controller.js
@@ -1,6 +1,34 @@
-import getTodosPosts from "../models/posts.model.js";
+import fs from 'fs';
+import {getTodosPosts, criarPost} from "../models/posts.model.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosPosts();
   res.status(200).json(posts);
+};
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+  try {
+    const posts = await criarPost(novoPost);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  };
+};
+
+export async function uploadImagem(req, res) {
+  const novoPost = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: ""
+  };
+
+  try {
+    const posts = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${posts.insertedId}.jpg`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ erro: error.message });
+  };
 };
--- src/models/posts.model.js
+++ src/models/posts.model.js
@@ -2,8 +2,14 @@ import conectarAoBanco from "../config/db.config.js";
 
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 
-export default async function getTodosPosts() {
+export async function getTodosPosts() {
   const db = conexao.db("imersao-alura");
   const colecao = db.collection("posts");
   return colecao.find().toArray();
 }
+
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-alura");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
+};
